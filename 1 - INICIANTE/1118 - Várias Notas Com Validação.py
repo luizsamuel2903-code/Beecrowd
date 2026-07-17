abbd36443afc9c0e4e0f92cd7972bc7485023e01
luizsamuel2903-code <luizsamuel2903@gmail.com>
2026-07-17 08:13:01 -0300

    modified:   "1 - INICIANTE/1118 - V\303\241rias Notas Com Valida\303\247\303\243o.py"

--- 1 - INICIANTE/1118 - Várias Notas Com Validação.py
+++ 1 - INICIANTE/1118 - Várias Notas Com Validação.py
@@ -1,3 +1,23 @@
-# -*- coding: utf-8 -*-
+notas = []
+fechar_programa = False
 
+while not fechar_programa:
+    nota = float(input())
+    if 0 <= nota <= 10: notas.append(nota)
+    else: print('nota invalida')
 
+    if len(notas) == 2: 
+        print(f'media = {sum(notas) / 2:.2f}')
+
+        while True:
+            print('novo calculo (1-sim 2-nao)')
+            novo_calculo = int(input())
+            
+            if novo_calculo == 1:
+                notas = []
+                break
+            elif novo_calculo == 2:
+                fechar_programa = True
+                break
+
+# 00 : 15 : 59, 17    
