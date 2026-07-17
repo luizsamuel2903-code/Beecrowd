e903a6a7564f46da55525d87effe55f2943a7b14
luizsamuel2903-code <luizsamuel2903@gmail.com>
2026-07-17 07:27:47 -0300

    new file:   1 - INICIANTE/1113 - Crescente e Decrescente.py

---
+++ 1 - INICIANTE/1114 - Senha Fixa.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+while True:
+    valor_tentativa = int(input())
+    if valor_tentativa == 2002:
+        print('Acesso Permitido')
+        break
+    print('Senha Invalida')
+
+# 00 : 02 : 29, 03
