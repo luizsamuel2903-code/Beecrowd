a2620d67752d37e4dea82f3dad883d2efc48fd4e
luizsamuel2903-code <luizsamuel2903@gmail.com>
2026-07-17 07:53:54 -0300

    new file:   "1 - INICIANTE/1117 - Valida\303\247\303\243o de Nota.py"

---
+++ 1 - INICIANTE/1117 - Validação de Nota.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+notas = []
+while True:
+    nota = float(input())
+    if 0 <= nota <= 10: notas.append(nota)
+    else: print('nota invalida')
+
+    if len(notas) >= 2: break
+
+media = sum(notas) / 2
+print(f'media = {media:.2f}')
