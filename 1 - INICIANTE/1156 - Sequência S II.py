e3b6272c83e1ea81c5d616f09a5fbf4febbbccf0
luizsamuel2903-code <luizsamuel2903@gmail.com>
2026-07-19 17:31:23 -0300

    modified:   "1 - INICIANTE/1156 - Sequ\303\252ncia S II.py"

--- 1 - INICIANTE/1156 - Sequência S II.py
+++ 1 - INICIANTE/1156 - Sequência S II.py
@@ -1,4 +1,10 @@
 # -*- coding: utf-8 -*-
 
 s = 0
-    
+denominador = 1
+for numerador in range(1, 39+1, 2):
+    s += numerador / denominador
+    denominador *= 2
+print(f'{s:.2f}')
+
+# 00 : 08 : 37, 95
