7223580eb271c166f73261a25d352229963ab22f
luizsamuel2903-code <luizsamuel2903@gmail.com>
2026-07-19 22:30:08 +0000

    Jul 19, 2026, 7:30 PM

--- 1 - INICIANTE/1160 - Crescimento Populacional.py
+++ 1 - INICIANTE/1160 - Crescimento Populacional.py
@@ -6,8 +6,9 @@ for _ in range(int(input())):
     g1, g2 = map(float, [g1, g2])
     
     anos = 0
-    while pa <= pb:
+    while pa <= pb and anos <= 100:
         pa = int(pa*(g1/100))
         pb = int(pb*(g2/100))
         anos += 1
-    print(anos)
+    if anos > 100: print('Mais de 1 seculo')
+    else: print(f'{anos} anos.')
