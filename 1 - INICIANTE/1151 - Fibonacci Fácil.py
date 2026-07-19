56ac026ef2557233b85c76b178e0354a59afcf64
luizsamuel2903-code <luizsamuel2903@gmail.com>
2026-07-19 19:55:55 +0000

    Jul 19, 2026, 4:55 PM

--- 1 - INICIANTE/1151 - Fibonacci Fácil.py
+++ 1 - INICIANTE/1151 - Fibonacci Fácil.py
@@ -5,9 +5,9 @@ n = int(input())
 termo_1, termo_2 = 0, 1
 
 for i in range(n):
-    if i == n-1: print(termo_1, end='')
-    else: print(termo_1, end=' ')
-    
+    if i == n-1: print(termo_1, end='-')
+    else: print(termo_1, end='+')
+
     soma_termos = termo_1 + termo_2
     termo_1 = termo_2
     termo_2 = soma_termos
