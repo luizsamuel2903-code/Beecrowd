7477993d55a8996f9a3fdde859c459b1ea072814
luizsamuel2903-code <luizsamuel2903@gmail.com>
2026-07-19 19:06:17 +0000

    Jul 19, 2026, 4:06 PM

--- 1 - INICIANTE/1150 - Ultrapassando Z.py
+++ 1 - INICIANTE/1150 - Ultrapassando Z.py
@@ -1,3 +1,14 @@
 # -*- coding: utf-8 -*-
 
+x = z = int(input())
+
+while z <= x:
+    z = int(input())
+
+a = []
+for i in range(x, z):
+    a.append(i)
+    if sum(a) >= z: break
+print(len(a))
+
 
