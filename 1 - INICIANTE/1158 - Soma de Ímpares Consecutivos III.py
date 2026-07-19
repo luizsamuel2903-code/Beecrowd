10268905933b213b0f6c80b3952fe98fd3f635cd
luizsamuel2903-code <luizsamuel2903@gmail.com>
2026-07-19 21:58:02 +0000

    Jul 19, 2026, 6:58 PM

--- 1 - INICIANTE/1158 - Soma de Ímpares Consecutivos III.py
+++ 1 - INICIANTE/1158 - Soma de Ímpares Consecutivos III.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 
-for i in range(int(input())):
+for _ in range(int(input())):
     x, y = map(int, input().split())
     if x % 2 == 0: x += 1 
 
     soma = 0
     for i in range(y):
-        x += 2
         soma += x
+        x += 2
     print(soma)
+    
