00328c065a6afd989eff23c3d3d40e9ccc218bab
luizsamuel2903-code <luizsamuel2903@gmail.com>
2026-07-19 22:23:05 +0000

    Jul 19, 2026, 7:23 PM

--- 1 - INICIANTE/1160 - Crescimento Populacional.py
+++ 1 - INICIANTE/1160 - Crescimento Populacional.py
@@ -1,3 +1,12 @@
 # -*- coding: utf-8 -*-
 
-
+for _ in range(int(input())):
+    pa, pb, g1, g2 = input().split()
+    pa, pb = map(int, [pa, pb])
+    g1, g2 = map(float, [g1, g2])
+    
+    anos = 0
+    while pa <= pb:
+        pa *= g1
+        pb *= g2
+        anos += 1
