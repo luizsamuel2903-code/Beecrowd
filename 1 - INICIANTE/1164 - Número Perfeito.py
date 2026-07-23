f9983e71eca56c6f08690af50d53ef935a9c1cd9
luizsamuel2903-code <luizsamuel2903@gmail.com>
2026-07-23 14:02:32 +0000

    Jul 23, 2026, 11:02 AM

--- 1 - INICIANTE/1164 - Número Perfeito.py
+++ 1 - INICIANTE/1164 - Número Perfeito.py
@@ -1,3 +1,13 @@
 # -*- coding: utf-8 -*-
 
-
+for _ in range(int(input())):
+    n = int(input())
+    d = []
+    for i in range(1, n+1):
+        if n % i == 0:
+            d.append(i)
+    if sum(d) - n == n:
+        print(f'{n} eh perfeito')
+    else:
+        print(f'{n} nao eh perfeito')
+        
