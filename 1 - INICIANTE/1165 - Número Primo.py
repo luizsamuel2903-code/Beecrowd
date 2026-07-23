d2c6d265b03721c09d822d51eaa73dc8f4fe2fed
luizsamuel2903-code <luizsamuel2903@gmail.com>
2026-07-23 14:07:27 +0000

    Jul 23, 2026, 11:07 AM

--- 1 - INICIANTE/1165 - Número Primo.py
+++ 1 - INICIANTE/1165 - Número Primo.py
@@ -1,4 +1,12 @@
 # -*- coding: utf-8 -*-
 
-
-
+for _ in range(int(input())):
+    n = int(input())
+    d = []
+    for i in range(1, n+1):
+        if n % i == 0:
+            d.append(i)
+    if len(d) == 2:
+        print(f'{n} eh primo')
+    else:
+        print(f'{n} nao eh primo')
