96dae6a2431978159e3f82806c891e6d38e63eeb
luizsamuel2903-code <luizsamuel2903@gmail.com>
2026-07-30 15:57:09 -0300

    modified:   Python/1 - INICIANTE/1181 - Linha na Matriz.py

--- Python/1 - INICIANTE/1181 - Linha na Matriz.py
+++ Python/1 - INICIANTE/1181 - Linha na Matriz.py
@@ -1,3 +1,9 @@
 # -*- coding: utf-8 -*-
 
-
+matriz = [[0 for _ in range(12)] for _ in range(12)]
+linha, operacao = int(input()), input()
+for i in range(12):
+    for j in range(12):
+        matriz[i][j] = float(input())
+if operacao == 'S': print(sum(matriz[linha]))
+if operacao == 'M': print(sum(matriz[linha])/12)
