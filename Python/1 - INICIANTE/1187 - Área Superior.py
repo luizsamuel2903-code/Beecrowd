696e8b2d8c4c6d7f6c68a3e973636b9e788d53c4
luizsamuel2903-code <luizsamuel2903@gmail.com>
2026-07-31 15:59:12 -0300

    modified:   "Python/1 - INICIANTE/1187 - \303\201rea Superior.py"

--- Python/1 - INICIANTE/1187 - Área Superior.py
+++ Python/1 - INICIANTE/1187 - Área Superior.py
@@ -1,3 +1,15 @@
 # -*- coding: utf-8 -*-
 
+command = input()
+matriz = [[0 for _ in range(12)] for _ in range(12)]
 
+number_of_elements = 0
+accumulator_of_values = 0
+for row in range(12):
+    for column in range(12):
+        value = float(input())
+        if column > row and column < (11-row):
+            number_of_elements += 1
+            accumulator_of_values += value
+if command == 'S': print(f'{accumulator_of_values:.1f}')
+if command == 'M': print(f'{accumulator_of_values/number_of_elements:.1f}')
