f6c19a5a6892445a3b18a279e5d9655c78a239de
luizsamuel2903-code <luizsamuel2903@gmail.com>
2026-08-06 11:16:29 -0300

    modified:   "Python/1 - INICIANTE/1190 - \303\201rea Direita.py" 	modified:   "Python/10 - TESTES/Visualiza\303\247\303\243o de Vareduras em Matrizes.py"

--- Python/1 - INICIANTE/1190 - Área Direita.py
+++ Python/1 - INICIANTE/1190 - Área Direita.py
@@ -5,4 +5,9 @@ c, a, e = input(), 0, 0
 for i in range(12):
     for j in range(12):
         v = float(input())
-        
+        if i+j>11 and i<6 or i<j and 5<i:
+            a += v
+            e += 1
+            
+if c == 'S': print(f'{a:.1f}')
+if c == 'M': print(f'{a/e:.1f}')
--- Python/10 - TESTES/Visualização de Vareduras em Matrizes.py
+++ Python/10 - TESTES/Visualização de Vareduras em Matrizes.py
@@ -3,8 +3,7 @@ m = [['[ ]' for _ in range(dm)] for _ in range(dm)]
 
 for i in range(dm):
     for j in range(dm):
-        #if : m[i][j] = '\033[34m[ ]\033[m'
-        #if i<j and 5<i : m[i][j] = '\033[32m[ ]\033[m'
-        ...
+        if i+j>11 and i<6: m[i][j] = '\033[34m[ ]\033[m'
+        if i<j and 5<i : m[i][j] = '\033[32m[ ]\033[m'
 
 for i in m: print(''.join(i))
